b12876b6e37d5d1455a204e14fe2bfc56bf37222
Your Name <a9119406541@gmail.com>
2022-06-24 15:31:37 +0300

    HW

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 18000;
+        int miles = ticketPrice / 20;
+        System.out.println("Вам начислят " + miles + " миль.");
+    }
+}
